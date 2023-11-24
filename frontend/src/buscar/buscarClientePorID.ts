ea49499d76bfbea73de32c3dc6694af6a7691587
Felipe Vieira <felipe.vieira30@fatec.sp.gov.br>
2023-11-24 10:09:13 -0300

    Adicionando lista 4

---
+++ frontend/src/buscar/buscarClientePorID.ts
@@ -0,0 +1,33 @@
+import { URI } from "../enuns/uri"
+import Busca from "./busca"
+
+/*export default class BuscarClientesPorID implements Busca {
+    public async buscar() {
+        let json = await fetch(URI.CLIENTE_POR_ID).then(resposta => resposta.json())        
+        return json
+    }
+}*/
+
+export const getClientePorIdUrl = (id: string): string => `http://localhost:32831/cliente/${id}`;
+
+export default class BuscarClientesPorID implements Busca {
+    private url: string;
+
+    constructor(id: string) {
+        this.url = getClientePorIdUrl(id);
+    }
+
+    public async buscar() {
+        try {
+            const response = await fetch(this.url);
+            if (!response.ok) {
+                throw new Error(`Erro ao buscar cliente por ID. Status: ${response.status}`);
+            }
+
+            return response.json();
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+}
